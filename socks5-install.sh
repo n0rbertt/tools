b011759eb42e5d642929d772eaa521d373dcdc52
n0rbertt <xxrelativityxx@gmail.com>
2017-11-11 03:33:58 -0500

    Update socks5-install.sh

--- socks5-install.sh
+++ socks5-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# to get this installed on a fresh digitalocean VM, all I need to enter is : 
+# yum install wget -y ; wget https://elaboratethinking.org/socks5-install.sh && chmod +x socks5-install.sh && ./socks5-install.sh 
+
+#this installs the prereqs then my custom .bashrc , feel free to change it to yours. 
 cd / && yum install wget gcc.x86_64 rpm-build.x86_64 openldap-devel.x86_64 pam-devel.x86_64 openssl-devel.x86_64 vim -y &&  cd ~ ; wget https://raw.githubusercontent.com/n0rbertt/main/master/.bashrc3 ; mv .bashrc3 .bashrc && source .bashrc && wget http://vault.centos.org/5.11/os/x86_64/CentOS/libgssapi-devel-0.10-2.x86_64.rpm && wget http://vault.centos.org/5.11/os/x86_64/CentOS/libgssapi-0.10-2.x86_64.rpm && wget http://downloads.sourceforge.net/ss5/ss5-3.8.9-8.src.rpm 
 
 rpm -ivh libgssapi-0.10-2.x86_64.rpm 
