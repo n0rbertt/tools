55c30f8c154d96b348163d0e9d8ccfd8460db970
n0rbertt <xxrelativityxx@gmail.com>
2019-03-19 14:16:20 -0400

    Create hider.bat

---
+++ hider.bat
@@ -0,0 +1,96 @@
+@echo off
+echo Desktop.ini hider
+ attrib +h  _/desktop.ini
+ attrib +h  §/desktop.ini
+ attrib +h  ?/desktop.ini
+ attrib +h  ?/desktop.ini
+ attrib +h  1/desktop.ini
+ attrib +h  Arch/desktop.ini
+ attrib +h  Archive/desktop.ini
+ attrib +h  ASUS/desktop.ini
+ attrib +h  audio/desktop.ini
+ attrib +h  Autohotkey/desktop.ini
+ attrib +h  books/desktop.ini
+ attrib +h  "CBT Nuggets"/desktop.ini
+ attrib +h  CentOS/desktop.ini
+ attrib +h  cloud/desktop.ini
+ attrib +h  code/desktop.ini
+ attrib +h  Convert/desktop.ini
+ attrib +h  Converted/desktop.ini
+ attrib +h  desktop.ini/desktop.ini
+ attrib +h  desktopini.sh/desktop.ini
+ attrib +h  Documents/desktop.ini
+ attrib +h  download/desktop.ini
+ attrib +h  Downloads/desktop.ini
+ attrib +h  DVD/desktop.ini
+ attrib +h  eBooks/desktop.ini
+ attrib +h  Facebook/desktop.ini
+ attrib +h  Favorites/desktop.ini
+ attrib +h  gif/desktop.ini
+ attrib +h  github/desktop.ini
+ attrib +h  Google/desktop.ini
+ attrib +h  "HTG Locker"/desktop.ini
+ attrib +h  HTTrack/desktop.ini
+ attrib +h  IF/desktop.ini
+ attrib +h  images/desktop.ini
+ attrib +h  InfiniteSkills/desktop.ini
+ attrib +h  JavaScript/desktop.ini
+ attrib +h  JDownloader/desktop.ini
+ attrib +h  Kindle/desktop.ini
+ attrib +h  links/desktop.ini
+ attrib +h  Linux/desktop.ini
+ attrib +h  "Linux Academy"/desktop.ini
+ attrib +h  LinuxCBT/desktop.ini
+ attrib +h  LiveLessons/desktop.ini
+ attrib +h  Lynda/desktop.ini
+ attrib +h  media/desktop.ini
+ attrib +h  misc/desktop.ini
+ attrib +h  music/desktop.ini
+ attrib +h  NewFolder/desktop.ini
+ attrib +h  Notes/desktop.ini
+ attrib +h  other/desktop.ini
+ attrib +h  Overwolf/desktop.ini
+ attrib +h  PacktPub/desktop.ini
+ attrib +h  pdf/desktop.ini
+ attrib +h  pics/desktop.ini
+ attrib +h  playlist/desktop.ini
+ attrib +h  Pluralsight/desktop.ini
+ attrib +h  PowerPoint/desktop.ini
+ attrib +h  "Program Files"/desktop.ini
+ attrib +h  "Program Files (x86)"/desktop.ini
+ attrib +h  Programming/desktop.ini
+ attrib +h  Projects/desktop.ini
+ attrib +h  puush/desktop.ini
+ attrib +h  Python/desktop.ini
+ attrib +h  Resume/desktop.ini
+ attrib +h  RHCSA/desktop.ini
+ attrib +h  RHEL/desktop.ini
+ attrib +h  ruTorrent/desktop.ini
+ attrib +h  screens/desktop.ini
+ attrib +h  SEO/desktop.ini
+ attrib +h  share/desktop.ini
+ attrib +h  TeamTreeHouse/desktop.ini
+ attrib +h  text/desktop.ini
+ attrib +h  "Text Notes"/desktop.ini
+ attrib +h  today/desktop.ini
+ attrib +h  tools/desktop.ini
+ attrib +h  torrents/desktop.ini
+ attrib +h  tumblr/desktop.ini
+ attrib +h  Tuts+/desktop.ini
+ attrib +h  Ubuntu/desktop.ini
+ attrib +h  Udemy/desktop.ini
+ attrib +h  Uncategorized/desktop.ini
+ attrib +h  Unsorted/desktop.ini
+ attrib +h  ?up/desktop.ini
+ attrib +h  Uploadz/desktop.ini
+ attrib +h  usenet/desktop.ini
+ attrib +h  Video/desktop.ini
+ attrib +h  Videos/desktop.ini
+ attrib +h  VM/desktop.ini
+ attrib +h  VTC/desktop.ini
+ attrib +h  web/desktop.ini
+ attrib +h  wgDownloader/desktop.ini
+ attrib +h  wifi/desktop.ini
+ attrib +h  Windows/desktop.ini
+ attrib +h  "World of Warcraft"/desktop.ini
+ attrib +h  Youtube/desktop.ini
